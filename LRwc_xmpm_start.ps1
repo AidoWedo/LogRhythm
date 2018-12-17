37fe98f6d62b111c4d7fb7e6d5bbe1c56b3ddb44
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2018-12-17 10:02:36 +0000

    Add files via upload

---
+++ LRwc_xmpm_start.ps1
@@ -0,0 +1,13 @@
+﻿# Richard Bevan
+# Starts Services for a standalone LogRhythm Web Console (i.e. installed on its own Server)
+# Use the other script LRwc_xmpm.ps1 if the web services are not stand alone
+# stop case API
+Start-service LogRhythmCaseAPI
+# stop ThreatIntelligence API
+Start-Service LogRhythmConsoleAPI
+# stop Web Console UI service
+Start-Service LogRhythmConsoleUI
+# stop LogRhythm Web Indexer
+Start-Service LogRhythmWebIndexer
+# stop LogRhythm Webservices Host API
+Start-Service LogRhythmWebServicesHostAPI
