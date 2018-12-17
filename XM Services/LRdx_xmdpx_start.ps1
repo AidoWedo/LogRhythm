a60d7301b1dd3b004e801fd203a921036f712a75
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2018-12-17 10:51:35 +0000

    Add files via upload

---
+++ XM Services/LRdx_xmdpx_start.ps1
@@ -0,0 +1,26 @@
+﻿# Richard Bevan
+# Starts Services for a Windows Based XM plus DX Services
+# This is for 7.4 so you may need to tweak it for any other version
+# Start Common Services First Service Registry, API Gateway, Authentication API, SQL Service, Windows Authentication Service, Admin API
+Start-Service LogRhythmServiceRegistry
+Start-Service LogRhythmAPIGateway
+Start-Services LogRhythmAuthenticationAPI
+Start-Service LogRhythmSQLService
+Start-Service LogRhythmWindowsAuthenticationService
+Start-Service LogRhythmAdminAPI
+# Start Metrics
+Start-Service LogRhythmMetricsDatabase
+Start-Service LogRhythmMericsCollection
+Start-Service LogRhythmMetricsWebUI
+# start DX Services assumption is that batch files are still in C:\Program Files\LogRhythm\Data Indexer\Tools if not change below
+cmd.exe /c 'C:\Program Files\LogRhythm\Data Indexer\tools\Start-all-services.bat'
+# True Identity this is optional if its not there it will error and move on
+start-service LogRhythmTrueIdentitySyncClient
+# start LR Legacy Services Mediator, AIE Comms Manager, AIE Drilldown Cache, AIEngine, Alarming and Response, Job Manager, LogRhythm Notification Service, finally System Monitor Agent
+Start-Service scmedsvr
+Start-Service LRAIEComMgr
+Start-Service LogRhythmAIEngineCacheDrilldown
+Start-Service LRAIEEngine
+Start-Service scarm
+Start-Service LogRhythmNotificationService
+Start-Service scsm
---
+++ XM Services/LRdx_xmdpx_stop.ps1
@@ -0,0 +1,26 @@
+﻿# Richard Bevan
+# Stops Services for a Windows Based XM plus DX Services
+# This is for 7.4 so you may need to tweak it for any other version
+# Stop Common Services First Service Registry, API Gateway, Authentication API, SQL Service, Windows Authentication Service, Admin API
+Stop-Service LogRhythmServiceRegistry
+Stop-Service LogRhythmAPIGateway
+Stop-Services LogRhythmAuthenticationAPI
+Stop-Service LogRhythmSQLService
+Stop-Service LogRhythmWindowsAuthenticationService
+Stop-Service LogRhythmAdminAPI
+# Stop Metrics
+Stop-Service LogRhythmMetricsDatabase
+Stop-Service LogRhythmMericsCollection
+Stop-Service LogRhythmMetricsWebUI
+# Stop DX Services assumption is that batch files are still in C:\Program Files\LogRhythm\Data Indexer\Tools if not change below
+cmd.exe /c 'C:\Program Files\LogRhythm\Data Indexer\tools\Stop-all-services.bat'
+# True Identity this is optional if its not there it will error and move on
+Stop-service LogRhythmTrueIdentitySyncClient
+# Stop LR Legacy Services Mediator, AIE Comms Manager, AIE Drilldown Cache, AIEngine, Alarming and Response, Job Manager, LogRhythm Notification Service, finally System Monitor Agent
+Stop-Service scmedsvr
+Stop-Service LRAIEComMgr
+Stop-Service LogRhythmAIEngineCacheDrilldown
+Stop-Service LRAIEEngine
+Stop-Service scarm
+Stop-Service LogRhythmNotificationService
+Stop-Service scsm
