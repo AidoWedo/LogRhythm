efa7e3aa61dd4083dec509b25977320b233ae5bf
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2018-12-17 10:03:15 +0000

    Add files via upload

---
+++ Web Console/LRwc_Standalone.ps1
@@ -0,0 +1,17 @@
+﻿# Richard Bevan
+# Starts Services for a standalone LogRhythm Web Console (i.e. installed on its own Server)
+# Use the other script LRwc_xmpm.ps1 if the web services are not stand alone
+# stop case API
+Stop-service LogRhythmCaseAPI
+# stop ThreatIntelligence API
+Stop-Service LogRhythmConsoleAPI
+# stop Web Console UI service
+Stop-Service LogRhythmConsoleUI
+# stop LogRhythm Web Indexer
+Stop-Service LogRhythmWebIndexer
+# stop LogRhythm Webservices Host API
+Stop-Service LogRhythmServicesHostAPI
+# stop LogRhythm API Gateway (NB standalone only)
+Stop-Service LogRhythmAPIGateway
+# stop LogRhythm Service Registry
+Stop-Service LogRhythmServiceRegistry
